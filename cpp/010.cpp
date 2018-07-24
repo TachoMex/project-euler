f9a270dba91181ccbd1c2b0a377a685f963d3e0e
Gilberto Vargas <tachoguitar@gmail.com>
2018-07-24 00:34:47 -0500

    Add solutions on cpp

--- cpp/010.cpp
+++ cpp/010.cpp
@@ -6,12 +6,16 @@ using std::cout;
 using std::endl;
 using number_theory::calc_sieve;
 
-const int64_t N = 1000000;
+const int64_t N = 2000000;
+int primes[N + 1];
+bool sieve[N + 1];
 
 int main(void) {
-  int primes[N];
-  bool sieve[N + 1];
   calc_sieve(sieve, primes, N);
-  cout << primes[10000] << endl;
+  int64_t sum = 0;
+  for (int i = 0; primes[i]; ++i) {
+    sum += primes[i];
+  }
+  cout << sum << endl;
   return 0;
 }
